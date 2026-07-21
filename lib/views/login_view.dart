ce047d9f4e728e791b6c7af1dd836ad1a644751e
kaydehoopz05 <keanid05@gmail.com>
2026-07-21 05:50:21 +0100

    Created login_view and imported other files

---
+++ lib/views/login_view.dart
@@ -0,0 +1,6 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/material.dart';
+import 'package:moodle/widgets/nav_drawer.dart';
+import 'package:moodle/constants.dart';
+import 'package:moodle/views/profile_page_view.dart';
