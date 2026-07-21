250e89860dfac76863a81d40d7d6bdaa372745ff
kaydehoopz05 <keanid05@gmail.com>
2026-07-21 05:54:42 +0100

    Created the basis for login page (appbar)

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -4,3 +4,67 @@ import 'package:flutter/material.dart';
 import 'package:moodle/widgets/nav_drawer.dart';
 import 'package:moodle/constants.dart';
 import 'package:moodle/views/profile_page_view.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+    @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: moodleWhite,
+        foregroundColor: moodleTextDark,
+        elevation: 1,
+        toolbarHeight: 76,
+        titleSpacing: 0,
+        leadingWidth: 56,
+        leading: Builder(
+          builder: (context) => IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () => Scaffold.of(context).openDrawer(),
+          ),
+        ),
+        title: Row(
+          children: [
+            GestureDetector(
+              onTap: () {
+                Navigator.pushReplacementNamed(context, '/');
+              },
+              child: Container(
+                width: 34,
+                height: 34,
+                decoration: BoxDecoration(
+                  color: moodleGrayBg,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Image.asset(
+                  'images/moodle_logo.png',
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+            const Text(
+              'Login',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+                color: moodlePurple,
+              ),
+            ),
+          ],
+        ),
+      ),
+      drawer: const NavDrawer(),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushReplacementNamed(context, '/profile');
+          },
+          child: const Text('Login'),
+        ),
+      ),
+    );
+  }
+
+}
