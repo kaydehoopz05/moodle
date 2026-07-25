f4b71d5c0c698356e3cfcc1720d69ccf6f7220c5
kaydehoopz05 <keanid05@gmail.com>
2026-07-25 07:58:41 +0100

    Created basis for database page

--- lib/views/ds_course_view.dart
+++ lib/views/ds_course_view.dart
@@ -4,3 +4,20 @@ import 'package:flutter/material.dart';
 import 'package:moodle/widgets/nav_drawer.dart';
 import 'package:moodle/constants.dart';
 import 'package:moodle/views/courses_view.dart';
+
+class DSCourseView extends StatelessWidget {
+  const DSCourseView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('M234 - School of Computing - Data Science Course'),
+      ),
+      drawer: const NavDrawer(),
+      body: const Center(
+        child: Text('Welcome to the Data Science Course!'),
+      ),
+    );
+  }
+}
