2543ff254e887f4af477929d05c3b4492e7f0669
kaydehoopz05 <keanid05@gmail.com>
2026-07-30 13:54:45 +0100

    Removed unneccessary import

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -3,7 +3,6 @@
 import 'package:flutter/material.dart';
 import 'package:moodle/widgets/nav_drawer.dart';
 import 'package:moodle/constants.dart';
-import 'package:moodle/views/profile_page_view.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({Key? key}) : super(key: key);
